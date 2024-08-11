04f22ee8fe7513eed0debb47833692c0a1e5c4c4
maryamsalarian <89162393+maryamsalarian@users.noreply.github.com>
2024-08-11 00:42:54 -0400

    Update Neo4jElements.java

--- Neo4jElements.java
+++ Neo4jElements.java
@@ -48,73 +48,123 @@ public class Neo4jElements implements AutoCloseable {
         }
     }
 
-    public void addRelationship(String actorId, String movieId) {
+    public String addRelationship(String actorId, String movieId) {
+        String returnStatement = "";
         try (Session session = driver.session()) {
-            session.writeTransaction(tx -> {
-                tx.run("MATCH (a:Actor {actorId: $actorId}), (m:Movie {movieId: $movieId})" +
-                                "MERGE (a)-[:ACTED_IN]->(m)",
-                        parameters("actorId", actorId, "movieId", movieId));
-                return null;
-            });
+            StatementResult result = session.run(
+                    "MATCH (a:Actor {actorId: $actorId}) " +
+                            "MATCH (m:Movie {movieId: $movieId}) " +
+                            "RETURN a.actorId AS actorId, m.movieId as movieId",
+                    parameters("actorId", actorId, "movieId", movieId)
+            );
+            if (!result.hasNext()) return "";
+        } catch (Neo4jException e) {
+            throw new RuntimeException("Failed to retrieve IDs: " + e.getMessage(), e);
+        }
+        try (Session session = driver.session()) {
+            session.run(
+                    "MATCH (a:Actor {actorId: $actorId}), (m:Movie {movieId: $movieId})" +
+                            "MERGE (a)-[:ACTED_IN]->(m)",
+                    Values.parameters("actorId", actorId, "movieId", movieId)
+            );
+            returnStatement = "Relationship added successfully";
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to add relationship: " + e.getMessage(), e);
         }
+        return returnStatement;
     }
 
-    public void addMovieRating(String movieId, int rating) {
+    public String addMovieRating(String movieId, int rating) {
+        String returnStatement = "";
         try (Session session = driver.session()) {
-            session.writeTransaction(tx -> {
-                tx.run("MATCH (m:Movie {movieId: $movieId}) " +
-                                "SET m.rating = $rating",
-                        parameters("movieId", movieId, "rating", rating));
-                return null;
-            });
+            StatementResult result = session.run(
+                    "MATCH (m:Movie {movieId: $movieId}) " +
+                            "RETURN m.movieId as movieId ",
+                    parameters("movieId", movieId)
+            );
+            if (!result.hasNext()) return "";
+        } catch (Neo4jException e) {
+            throw new RuntimeException("Failed to retrieve movie ID: " + e.getMessage(), e);
+        }
+        try (Session session = driver.session()) {
+            session.run(
+                    "MATCH (m:Movie {movieId: $movieId}) " +
+                            "SET m.rating = $rating",
+                    Values.parameters("movieId", movieId, "rating", rating)
+            );
+            returnStatement = "Movie Rating added successfully";
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to add movie rating: " + e.getMessage(), e);
         }
+        return returnStatement;
     }
 
-    public void addMovieYear(String movieId, int year) {
+    public String addMovieYear(String movieId, int year) {
+        String returnStatement = "";
         try (Session session = driver.session()) {
-            session.writeTransaction(tx -> {
-                tx.run("MATCH (m:Movie {movieId: $movieId}) " +
-                                "SET m.year = $year",
-                        parameters("movieId", movieId, "year", year));
-                return null;
-            });
+            StatementResult result = session.run(
+                    "MATCH (m:Movie {movieId: $movieId}) " +
+                            "RETURN m.movieId as movieId",
+                    parameters("movieId", movieId)
+            );
+            if (!result.hasNext()) return "";
+        } catch (Neo4jException e) {
+            throw new RuntimeException("Failed to retrieve movie ID: " + e.getMessage(), e);
+        }
+        try (Session session = driver.session()) {
+            session.run(
+                    "MATCH (m:Movie {movieId: $movieId})" +
+                            "SET m.year = $year",
+                    Values.parameters("movieId", movieId, "year", year)
+            );
+            returnStatement = "Movie year added successfully";
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to add movie year: " + e.getMessage(), e);
         }
+        return returnStatement;
     }
 
-    public void addActorAward(String actorId, String award) {
+    public String addActorAward(String actorId, String award) {
+        String returnStatement = "";
         try (Session session = driver.session()) {
-            session.writeTransaction(tx -> {
-                tx.run("MATCH (a:Actor {actorId: $actorId}) " +
-                                "SET a.award = $award",
-                        parameters("actorId", actorId, "award", award));
-                return null;
-            });
+            StatementResult result = session.run(
+                    "MATCH (a:Actor {actorId: $actorId}) " +
+                            "RETURN a.actorId AS actorId",
+                    parameters("actorId", actorId)
+            );
+            if (!result.hasNext()) return "";
         } catch (Neo4jException e) {
-            throw new RuntimeException("Failed to add award to actor: " + e.getMessage(), e);
+            throw new RuntimeException("Failed to retrieve actor ID: " + e.getMessage(), e);
         }
+        try (Session session = driver.session()) {
+            session.run(
+                    "MATCH (a:Actor {actorId: $actorId}) " +
+                                "SET a.award = $award",
+                    Values.parameters("actorId", actorId, "award", award)
+            );
+            returnStatement = "Actor award added successfully";
+        } catch (Neo4jException e) {
+            throw new RuntimeException("Failed to add actor award: " + e.getMessage(), e);
+        }
+        return returnStatement;
     }
 
     public String getActor(String actorId) {
         StringBuilder actorInfo = new StringBuilder();
         try (Session session = driver.session()) {
             StatementResult result = session.run(
-                    "MATCH (a:Actor {actorId: $actorId})-[:ACTED_IN]->(m:Movie) " +
+                    "MATCH (a:Actor {actorId: $actorId}) " +
+                            "OPTIONAL MATCH (a)-[:ACTED_IN]->(m:Movie) " +
                             "RETURN a.actorId AS actorId, a.name AS name, collect(m.movieId) AS movies",
                     parameters("actorId", actorId)
             );
             if (result.hasNext()) {
                 Record record = result.next();
-                actorInfo.append("Actor ID: ").append(record.get("actorId").asString()).append("\n")
-                        .append("Name: ").append(record.get("name").asString()).append("\n")
-                        .append("Movies: ").append(String.join(", ", record.get("movies").asList(Values.ofString()))).append("\n");
+                actorInfo.append("actorId: ").append(actorId).append("\n")
+                        .append("name: ").append(record.get("name").asString()).append("\n")
+                        .append("movies: ").append(String.join(", ", record.get("movies").asList(Values.ofString()))).append("\n");
             } else {
-                actorInfo.append("Actor not found.");
+                return "";
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve actor: " + e.getMessage(), e);
@@ -126,17 +176,18 @@ public class Neo4jElements implements AutoCloseable {
         StringBuilder movieInfo = new StringBuilder();
         try (Session session = driver.session()) {
             StatementResult result = session.run(
-                    "MATCH (m:Movie {movieId: $movieId})<-[:ACTED_IN]-(a:Actor) " +
+                    "MATCH (m:Movie {movieId: $movieId}) " +
+                            "OPTIONAL MATCH (m)<-[:ACTED_IN]-(a:Actor) " +
                             "RETURN m.movieId AS movieId, m.name AS name, collect(a.actorId) AS actors",
                     parameters("movieId", movieId)
             );
             if (result.hasNext()) {
                 Record record = result.next();
-                movieInfo.append("Movie ID: ").append(record.get("movieId").asString()).append("\n")
-                        .append("Name: ").append(record.get("name").asString()).append("\n")
-                        .append("Actors: ").append(String.join(", ", record.get("actors").asList(Values.ofString()))).append("\n");
+                movieInfo.append("movieId: ").append(movieId).append("\n")
+                        .append("name: ").append(record.get("name").asString()).append("\n")
+                        .append("actors: ").append(String.join(", ", record.get("actors").asList(Values.ofString()))).append("\n");
             } else {
-                movieInfo.append("Movie not found.");
+                return "";
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve movie: " + e.getMessage(), e);
@@ -155,11 +206,11 @@ public class Neo4jElements implements AutoCloseable {
             );
             if (result.hasNext()) {
                 Record record = result.next();
-                resultInfo.append("Actor ID: ").append(record.get("actorId").asString()).append("\n")
-                        .append("Movie ID: ").append(record.get("movieId").asString()).append("\n")
-                        .append("Has Relationship: ").append(record.get("hasRelationship").asBoolean()).append("\n");
+                resultInfo.append("actorId: ").append(actorId).append("\n")
+                        .append("movieId: ").append(movieId).append("\n")
+                        .append("hasRelationship: ").append(record.get("hasRelationship").asBoolean()).append("\n");
             } else {
-                resultInfo.append("Relationship not found.");
+                return "";
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to check relationship: " + e.getMessage(), e);
@@ -167,7 +218,7 @@ public class Neo4jElements implements AutoCloseable {
         return resultInfo.toString().trim();
     }
 
-    public int computeBaconNumber(String targetActorId) {
+    public String computeBaconNumber(String targetActorId) {
         String kevinBaconId = "";
         try (Session session = driver.session()) {
             StatementResult result = session.run(
@@ -175,14 +226,17 @@ public class Neo4jElements implements AutoCloseable {
             );
             if (result.hasNext()) {
                 kevinBaconId = result.single().get("actorId").asString();
+            } else {
+                return "";
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve Kevin Bacon's ID: " + e.getMessage(), e);
         }
         if (targetActorId.equals(kevinBaconId)) {
-            return 0;
+            return "baconNumber: 0";
         }
         int baconNumber = -1;
+        StringBuilder resultInfo = new StringBuilder();
         try (Session session = driver.session()) {
             StatementResult result = session.run(
                     "MATCH (bacon:Actor {name: 'Kevin Bacon'}), (target:Actor {actorId: $targetActorId}) " +
@@ -193,11 +247,12 @@ public class Neo4jElements implements AutoCloseable {
             if (result.hasNext()) {
                 Record record = result.single();
                 baconNumber = record.get("baconNumber").asInt();
+                resultInfo.append("baconNumber: ").append(baconNumber);
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to compute Bacon number: " + e.getMessage(), e);
         }
-        return baconNumber;
+        return resultInfo.toString().trim();
     }
 
     public String computeBaconPath(String targetActorId) {
@@ -208,12 +263,14 @@ public class Neo4jElements implements AutoCloseable {
             );
             if (result.hasNext()) {
                 kevinBaconId = result.single().get("actorId").asString();
+            } else {
+                return "";
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve Kevin Bacon's ID: " + e.getMessage(), e);
         }
         if (targetActorId.equals(kevinBaconId)) {
-            return "[" + kevinBaconId + "]";
+            return "baconPath: " + kevinBaconId;
         }
         StringBuilder baconPath = new StringBuilder();
         try (Session session = driver.session()) {
@@ -224,12 +281,12 @@ public class Neo4jElements implements AutoCloseable {
                             "RETURN [node IN path_nodes | CASE " +
                             "    WHEN node:Actor THEN node.actorId " +
                             "    WHEN node:Movie THEN node.movieId " +
-                            "END] AS baconPath",
+                            "END] AS baconPath LIMIT 1",
                     Values.parameters("targetActorId", targetActorId)
             );
             if (result.hasNext()) {
                 Record record = result.single();
-                baconPath.append(record.get("baconPath").asList().toString());
+                baconPath.append("baconPath: ").append(String.join(", ", record.get("baconPath").asList(Values.ofString())));
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to compute Bacon path: " + e.getMessage(), e);
@@ -238,20 +295,22 @@ public class Neo4jElements implements AutoCloseable {
     }
 
     public String getMoviesAboveRating(int rating) {
-        StringBuilder movieIds = new StringBuilder();
+        StringBuilder moviesAboveRating = new StringBuilder();
         try (Session session = driver.session()) {
             StatementResult result = session.run(
                     "MATCH (m:Movie) WHERE m.rating > $rating RETURN m.movieId AS movieId",
                     Values.parameters("rating", rating)
             );
+            if (!result.hasNext()) return "";
+            moviesAboveRating.append("rating: ").append(rating).append("\n");
             while (result.hasNext()) {
                 Record record = result.next();
-                movieIds.append(record.get("movieId").asString()).append("\n");
+                moviesAboveRating.append("movies: ").append(record.get("movieId").asString()).append("\n");
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve movies: " + e.getMessage(), e);
         }
-        return movieIds.toString().trim();
+        return moviesAboveRating.toString().trim();
     }
 
     public String getMoviesByYear(int year) {
@@ -261,9 +320,11 @@ public class Neo4jElements implements AutoCloseable {
                     "MATCH (m:Movie) WHERE m.year = $year RETURN m.movieId AS movieId",
                     Values.parameters("year", year)
             );
+            if (!result.hasNext()) return "";
+            movieIds.append("year: ").append(year).append("\n");
             while (result.hasNext()) {
                 Record record = result.next();
-                movieIds.append(record.get("movieId").asString()).append("\n");
+                movieIds.append("movies: ").append(record.get("movieId").asString()).append("\n");
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve movies: " + e.getMessage(), e);
@@ -278,9 +339,11 @@ public class Neo4jElements implements AutoCloseable {
                     "MATCH (a:Actor) WHERE a.award = $award RETURN a.actorId AS actorId",
                     Values.parameters("award", award)
             );
+            if (!result.hasNext()) return "";
+            actorIds.append("award: ").append(award).append("\n");
             while (result.hasNext()) {
                 Record record = result.next();
-                actorIds.append(record.get("actorId").asString()).append("\n");
+                actorIds.append("actors: ").append(record.get("actorId").asString()).append("\n");
             }
         } catch (Neo4jException e) {
             throw new RuntimeException("Failed to retrieve actors by award: " + e.getMessage(), e);
